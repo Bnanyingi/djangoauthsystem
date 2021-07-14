96d58adc556606f4e5dcd75f49742fd5e9dc4348
Bnanyingi <58061814+Bnanyingi@users.noreply.github.com>
2021-07-14 20:50:43 +0300

    update

--- authsysproject/users/views.py
+++ authsysproject/users/views.py
@@ -14,7 +14,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Hi {username}, your account was created successfully')
+            messages.success(request, 'Hi {username}, your account was created successfully')
             return redirect('home')
     else:
         form = UserRegisterForm()    
