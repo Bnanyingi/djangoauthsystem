690b99a3b7a00206d057b035ac5c36c4bfc7e4c3
Bnanyingi <58061814+Bnanyingi@users.noreply.github.com>
2021-07-14 20:41:53 +0300

    formsupdate

--- authsysproject/users/views.py
+++ authsysproject/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 # Create your views here.
@@ -9,13 +10,13 @@ def home(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm()
+        form = UserRegisterForm()
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Hi {username}, your account was created successfully')
             return redirect('home')
     else:
-        form = UserCreationForm()    
+        form = UserRegisterForm()    
     
     return render(request, 'users/register.html', {'form': form})
