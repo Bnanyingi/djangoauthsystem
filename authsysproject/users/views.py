3e652d72a30c58376b3eba7066b40c9ee6b3b32b
Bnanyingi <58061814+Bnanyingi@users.noreply.github.com>
2021-07-15 19:22:09 +0300

    fixupdate

--- authsysproject/users/views.py
+++ authsysproject/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -20,3 +21,7 @@ def register(request):
         form = UserRegisterForm()    
     
     return render(request, 'users/register.html', {'form': form})
+
+@login_required()
+def profile(request):
+    return render(request, 'users/register.html')
